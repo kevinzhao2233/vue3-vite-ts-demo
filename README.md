5d58989972a72ba52e1598bf87cf59786525d339
kevinzhao2233 <kevin_2233@qq.com>
2021-02-25 11:08:27 +0800

    ✨ 添加 echarts

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## 计划
 - [x] vuex
-- [ ] axios
-- [ ] fetch Api
-- [ ] swiper
-- [ ] echarts
+- [x] axios
+- [ ] swiper  适配不佳，会出现栈溢出的问题
+- [x] echarts
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,18 +7,24 @@
     <template #header> 使用 axios 搜索网易云歌曲 </template>
     <Music />
   </el-card>
+  <el-card class="card" shadow="hover">
+    <template #header> 使用 Echarts </template>
+    <Echarts />
+  </el-card>
 </template>
 
 <script>
   import { defineComponent } from 'vue'
   import Counter from './components/Counter.vue'
   import Music from './components/Music.vue'
+  import Echarts from './components/Echarts.vue'
 
   export default defineComponent({
     name: 'Home',
     components: {
       Counter,
-      Music
+      Music,
+      Echarts
     },
     setup() {
       return {}
