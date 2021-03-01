72e550a5b8473fbb680beab315bbd5f2104fd568
kevinzhao2233 <kevin_2233@qq.com>
2021-03-01 11:10:15 +0800

    🔧 细化部分配置

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+/**
+ * Vite 中文文档 https://cn.vitejs.dev/
+ */
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
@@ -5,11 +8,11 @@ module.exports = {
   server: {
     // 开发服务端口
     port: 8111,
-    // 是否开启 https, 开启了也没证书？？
+    // 是否开启 https, 但是开启了也没有证书，只在有些功能必须要 https 协议时使用即可
     https: false
   },
   resolve: {
-    // 别名，别名映射，2.0 版本不再需要以 / 结尾了
+    // 别名映射，2.0 版本不再需要以 / 结尾了
     alias: {
       '/@': path.resolve(__dirname, './src'),
       '/@comps': path.resolve(__dirname, './src/components'),
