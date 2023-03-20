9e8976a323869cbf51eb2c2d792d5efbd7de8184
赵凯文 <kevin_2233@qq.com>
2023-03-20 23:05:41 +0800

    🔥 删除了旧版的 axios 封装

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,5 +1,9 @@
 import AxiosCls from './AxiosCls';
 
+/**
+ * 通用的 axios 实例
+ * 建议为每一个后端服务或未使用相同请求相应格式的后端服务都创建不同的 axios 实例
+ */
 export const basicInstance = new AxiosCls({
   baseURL: 'https://www.fastmock.site/mock/855d70fd0fb848b6abd6c1a945e7834b/api-test',
   timeout: 1000 * 60 * 5,
